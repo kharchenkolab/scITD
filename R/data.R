288e82c2fd0537219f51ed9efc14ea25157f6c5b
j-mitchel <jonathan.mitchel3@gmail.com>
2020-10-13 22:54:50 -0400

    updated data description

--- R/data.R
+++ R/data.R
@@ -1,15 +1,16 @@
-#' 
+#' Ensemble to gene symbol mapping
 #'
 "genes"
 
-#' 
+#' Gene expression UMI counts matrix for all cells and all genes
 #'
 "pbmc_sub_counts"
 
-#'
+#' Cell-wise meta data variables in a data.frame
 #'
 "pbmc_sub_meta"
 
-#' 
-#' 
+#' Gene expression UMI counts matrix after normalizing for library size
+#' and log transforming values
+#'
 "pbmc_sub_transformed"
