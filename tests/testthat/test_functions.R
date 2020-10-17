f4b58cb933e0efb7e597a236f93cb30dcbe21f70
j-mitchel <jonathan.mitchel3@gmail.com>
2020-10-17 16:08:18 -0400

    fixed colMeanVars test

--- tests/testthat/test_functions.R
+++ tests/testthat/test_functions.R
@@ -1,9 +1,10 @@
 library(scITD)
 library(testthat)
+library(Matrix)
 
 test_that("colMeanVars() functionality", {
   donor_by_gene <- rbind(c(9,2,1,5),c(3,3,1,2))
-  donor_by_gene <- methods::as(donor_by_gene,'sparseMatrix')
+  donor_by_gene <- Matrix(donor_by_gene, sparse = TRUE)
   result <- colMeanVars(donor_by_gene,rowSel = NULL)
   expected_result <- cbind(c(6,2.5,1,3.5),c(9,.25,0,2.25),c(2,2,2,2))
   expected_result <- as.data.frame(expected_result)
